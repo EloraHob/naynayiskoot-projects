8d133f1b3d62574a88e7888ff9145c823a6ea4c6
elorahob <elorahob@gmail.com>
2024-02-14 10:41:04 -0500

    Update file structure: integrate quiz into main-site directory

--- main-site/src/app/page.js
+++ main-site/src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
+        <Link href='/quiz'>Take the Quiz</Link>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -29,14 +31,7 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        Put Stuff Here. 
       </div>
 
       <div className={styles.grid}>
---
+++ main-site/src/app/quiz/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function QuizLayout() {
+    return (
+        <div className={styles.div}>
+            Stuff goes here!
+        </div>
+    );
+}
