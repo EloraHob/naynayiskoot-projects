1a85c97df19314a6d0a0f0bf8bea66f8641bad81
elorahob <elorahob@gmail.com>
2024-02-19 22:45:41 -0500

    fix pic-otd page and css files

--- main-site/src/app/pic-of-the-day/page.js
+++ main-site/src/app/pic-of-the-day/page.js
@@ -31,42 +31,7 @@ export default function Home() {
                         />
                     </div>
                 </section>
-
-                <section className={styles.gamesSection}>
-                    <h2>Mini Games</h2>
-                    <div className={styles.gamesContainer}>
-                        <div className={styles.gameCard}>
-                            <div className={styles.gameImage}>
-                                <Image src="/naynay-pics/1.jpg" alt="Farting Rat Music" layout="fill" objectFit="cover" />
-                            </div>
-                            <div className={styles.gameTitle}>Fart Rat Music (coming soon)</div>
-                        </div>
-                        <div className={styles.gameCard}>
-                            <div className={styles.gameImage}>
-                                <Image src="/naynay-pics/1.jpg" alt="Doggy Dress-Up Game" layout="fill" objectFit="cover" />
-                            </div>
-                            <div className={styles.gameTitle}>Doggy Dress-Up Game (coming soon)</div>
-                        </div>
-                        <div className={styles.gameCard}>
-                            <div className={styles.gameImage}>
-                                <Image src="/naynay-pics/1.jpg" alt="Spy x Family personality quiz" layout="fill" objectFit="cover" />
-                            </div>
-                            <div className={styles.gameTitle}>Which Spy x Family Character Are You? (coming soon)</div>
-                        </div>
-                        <div className={styles.gameCard}>
-                            <div className={styles.gameImage}>
-                                <Image src="/naynay-pics/1.jpg" alt="Sundae Scoop Game" layout="fill" objectFit="cover" />
-                            </div>
-                            <div className={styles.gameTitle}>Sundae Scoop (coming soon)</div>
-                        </div>
-                    </div>
-                </section>
-
             </main>
-
-            <footer className={styles.footer}>
-                <p>&copy; 2024 NayNayIsKoot. All rights reserved.</p>
-            </footer>
         </div>
     );
 }
