a577549d15484f8dafcfa9e224448f61c6cae016
elorahob <elorahob@gmail.com>
2024-02-20 15:00:02 -0500

    rename component files

--- main-site/src/app/components/Header.jsx
+++ main-site/src/app/components/Header.jsx
@@ -1,6 +1,6 @@
 import styles from "../page.module.css"
 
-export default function Navbar() {
+export default function Header() {
     return (
         <header className={styles.header}>
             <h1>nay nay is koot.</h1>
@@ -14,3 +14,10 @@ export default function Navbar() {
         </header>
     );
 }
+
+/* CONSIDERATIONS AND NOTES
+
+Taking inspiration from the design of https://neal.fun/, I think I just want the nav/header to display 
+the site logo (aligned left when minigame is open, centered in large text on homepage otherwise). For now, just going 
+to stick to this version across the whole site. 
+*/
--- main-site/src/app/layout.js
+++ main-site/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from './components/Footer'; 
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Header />
         {children}
         <Footer />
       </body>
