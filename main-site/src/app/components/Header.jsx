bfa4bebb88d7b07a30cb8bdc3d3baaa5ba5edb44
elorahob <elorahob@gmail.com>
2024-02-21 00:17:43 -0500

    make header logo-img responsive

--- main-site/src/app/components/Header.jsx
+++ main-site/src/app/components/Header.jsx
@@ -1,16 +1,16 @@
-import styles from "../globals.css"
+import styles from "./styles/Header.module.css"
 import Image from 'next/image';
 
-// TODO: acquire higher quality logo file and make responsive. 
+// TODO: acquire higher quality logo file. 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div>
+            <div className={styles.logo}>
                 <Image
                     src="/logo.png"
                     alt="nay nay is koot. logo"
                     width={200}
-                    height={230}
+                    height={260}
                     priority
                 />
             </div>
@@ -23,11 +23,16 @@ export default function Header() {
 Taking inspiration from the design of https://neal.fun/, I think I just 
 want the nav/header to display the site logo (aligned left when minigame 
 is open, centered in large text on homepage otherwise). For now, just going 
-to stick to this version across the whole site. 
+to stick to this version until I have an actual minigame finished. 
 
 I almost deleted this file... after some thought, I wondered if I even needed 
 a header component file since there isn't any real site navigation needed and 
 the content just appears on the homepage. In the future, I might need to modify 
 this to accomodate navigation to different kinds of content on the site (such as 
 quizzes, games, statistics, etc.)... but for now I'm going to keep it simple.
+
+Making the Next/Image responsive was needlessly difficult. I'd probably have 
+finished working on the css in half the time if Next/Image wasn't so finicky. 
+The height and width props must be defined in the Image tag but they can be 
+overriden in CSS. Special terms and conditions apply. 
 */
